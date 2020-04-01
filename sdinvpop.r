6c3410843e6b0c0885f46425ff6b61c9d2d344cd
xujinghua <jinghua.xu@student.uni-tuebingen.de>
2020-04-01 23:30:51 +0200

    m2.3 to go

--- sdinvpop.r
+++ sdinvpop.r
@@ -17,7 +17,7 @@ ggplot(d, aes(x=population, y=nPhonemes)) +
   scale_x_log10() +
   scale_y_log10()
 
-# Linear models
+## Linear models
 
 
 "A simple linear regression with log(population size) as independent variable and log(sound inventory size) as dependent variable(non-bayesian)"
@@ -308,32 +308,95 @@ for ( i in 1:100 )
 "Absolutely no sign of a corelation between the 2 variables with 2 random effects being considered at the same time. Given the observations from previous 3 models, we can draw the conclusion that the corelation between the 2 variable of intrests is fake, at least not linear."
 
 
-# Poisson models
+## Poisson models
 
-################
-"a simple Poisson regression with log(population size) as independent variable and log(sound inventory size) as dependent variable"
+# prior predictive checks
 
-dat2.0 = list(
-  lpop = dd$log_pop,
-  lnp = dd$log_np)
+# determine prior alpha
+
+mean(d$nPhonemes)
+
+curve( dlnorm( x , 0 , 10 ) , from=0 , to=100 , n=200 )
+curve( dlnorm( x , 0 , 1.5 ) , from=0 , to=100 , n=200 )
+curve( dlnorm( x , 3 , 0.5 ) , from=0 , to=100 , n=200 )
+# optimal prior
+curve( dlnorm( x , 3.5 , 0.5 ) , from=0 , to=100 , n=200 )
+
+
+a <- rnorm(1e4,0,10)
+lambda <- exp(a)
+mean( lambda )
+
+a <- rnorm(1e4, 3, 0.5)
+lambda <- exp(a)
+mean( lambda )
+
+# optimal prior: close to mean(d$nPhonemes)
+a <- rnorm(1e4, 3.5, 0.5)
+lambda <- exp(a)
+mean( lambda )
+
+
+# determine prior beta
+set.seed(10)
+N <- 100
+a <- rnorm( N , 3.5 , 0.5 )
+b <- rnorm( N , 0 , 0.2 )
+plot( NULL , xlim=c(-2,2) , ylim=c(0,100) )
+for ( i in 1:N ) curve( exp( a[i] + b[i]*x ) , add=TRUE , col=col.alpha("black",0.5) )
+
+
+"a simple Poisson regression without randowm effects."
+dat2.0 <- list(
+  pop = dd$log_pop_std,
+  np = d$nPhonemes
+)
 
 m2.0 <- ulam(
   alist(
-    lnp ~ dpois(lambda),
-    log(lambda) <- a + b*lpop,
-    # conventional priors
-    a ~ dnorm(a_bar, a_sigma),
-    a_bar ~ dnorm(0, 1.5),
-    b ~ dnorm(b_bar, b_sigma),
-    b_bar ~ dnorm(0, 1.5),
-    a_sigma ~ dexp(1),
-    b_sigma ~ dexp(1),
-    sigma ~ dexp(1)
+    np ~ dpois(lambda),
+    log(lambda) <- a + b*pop,
+    # optimal priors
+    a ~ dnorm(3.5, 0.5),
+    b ~ dnorm(0, 0.2)
   ), data = dat2.0, chains = 4, log_lik = TRUE)
 
 precis(m2.0, depth = 2)
-WAIC(m2.0)
-#####################
+
+# posterior
+k <- LOOPk(m2.0)
+plot( dd$log_pop_std , d$nPhonemes , xlab="standardized log population" , ylab="nPhonemes" ,
+      col=rangi2 , pch=1 , lwd=2 ,
+      ylim=c(0,75) , cex=1+normalize(k) )
+# set up the horizontal axis values to compute predictions at
+ns <- 100
+P_seq <- seq( from=-1.4 , to=3 , length.out=ns )
+
+lambda <- link( m2.0 , data=data.frame( pop=P_seq) )
+lmu <- apply( lambda , 2 , mean )
+lci <- apply( lambda , 2 , PI )
+lines( P_seq , lmu , lty=2 , lwd=1.5 )
+shade( lci , P_seq , xpd=TRUE )
+
+# posterior
+post2.0 <- extract.samples( m2.0 )
+post2.0
+
+# display raw data and sample size
+plot( dd$log_pop_std , d$nPhonemes ,
+      xlim=range(dd$log_pop_std) , ylim=range(d$nPhonemes) ,
+      col=rangi2 , xlab="standardized log population" , ylab="nPhonemes" )
+mtext(concat("N = ",100))
+
+# plot the lines, with transparency
+for ( i in 1:100 )
+  curve( exp(post2.0$a[i] + post2.0$b[i]*x),
+         col=col.alpha("black",0.3) , add=TRUE )
+
+
+"First impression from simple poisson model with no random effects: a corelation can be seen in the posterior plot."
+
+
 
 "a hierarchical model with language families as random effect(a Poisson regression instead of a linear regression)"
 
@@ -347,21 +410,55 @@ m2.1 <- ulam(
   alist(
     np ~ dpois(lambda),
     log(lambda) <- a[lf] + b[lf]*pop,
-    # conventional priors
-    a[lf] ~ dnorm(a_bar, a_sigma),
-    a_bar ~ dnorm(0, 1.5),
-    b[lf] ~ dnorm(b_bar, b_sigma),
-    b_bar ~ dnorm(0, 1.5),
-    a_sigma ~ dexp(1),
-    b_sigma ~ dexp(1),
-    sigma ~ dexp(1)
+    # optimal priors
+    a[lf] ~ dnorm(3.5, 0.5),
+    b[lf] ~ dnorm(0, 0.2)
   ), data = dat2.1, chains = 4, log_lik = TRUE)
 
+
+
 precis(m2.1, depth = 2)
+# fine n_eff values about the sample size and fine Rhat values appraoching 1
 WAIC(m2.1)
-compare(m1.1, m1.2, m1.3, m2.1)
+
 
 # posterior
+post2.1 <- extract.samples( m2.1 )
+post2.1
+
+# display raw data and sample size
+plot( dd$log_pop_std , d$nPhonemes ,
+      xlim=range(dd$log_pop_std) , ylim=range(d$nPhonemes) ,
+      col=rangi2 , xlab="standardized log population" , ylab="standardized sound inventary size" )
+mtext(concat("N = ",100))
+
+# change lf(1:5) to alter language family(random effect control)
+lf = 5
+
+# plot the lines, with transparency
+for ( i in 1:100 )
+  curve( exp(post2.1$a[lf,i] + post2.1$b[lf,i]*x),
+         col=col.alpha("black",0.3) , add=TRUE )
+
+
+
+# posterior
+k <- LOOPk(m2.1)
+plot( dd$log_pop_std , d$nPhonemes , xlab="standardized log population" , ylab="nPhonemes" ,
+      col=rangi2 , pch =ifelse( as.integer(dd$glottologFamily)==5 , 19 , 3 ), lwd=1 ,
+      ylim=c(0,75) , cex=1+normalize(k) )
+
+# set up the horizontal axis values to compute predictions at
+ns <- 100
+P_seq <- seq( from=-1.4 , to=3 , length.out=ns )
+
+# change lf to alter language family(random effect control)
+lambda <- link( m2.1 , data=data.frame( pop=P_seq , lf=5 ) )
+lmu <- apply( lambda , 2 , mean )
+lci <- apply( lambda , 2 , PI )
+lines( P_seq , lmu , lty=2 , lwd=1.5 )
+shade( lci , P_seq , xpd=TRUE )
+
 
 "a hierarchical model with continents as random effect(a Poisson regression instead of a linear regression)"
 
@@ -374,20 +471,51 @@ dat2.2 <- list(
 m2.2 <- ulam(
   alist(
     np ~ dpois(lambda),
-    log(lambda) <- a[cid] + b[cid]*np,
-    # conventional prior
-    a[cid] ~ dnorm(a_bar, a_sigma),
-    a_bar ~ dnorm(0, 1.5),
-    b[cid] ~ dnorm(b_bar, b_sigma),
-    b_bar ~ dnorm(0, 1.5),
-    a_sigma ~ dexp(1),
-    b_sigma ~ dexp(1),
-    sigma ~ dexp(1)
+    log(lambda) <- a[cid] + b[cid]*pop,
+    # optimal priors
+    a[cid] ~ dnorm(3.5, 0.5),
+    b[cid] ~ dnorm(0, 0.2)
   ), data = dat2.2, chains = 4, log_lik = TRUE)
 
 precis(m2.2, depth = 2)
+# fine n_eff values and Rhat values
 WAIC(m2.2)
-compare(m1.1, m1.2, m1.3, m2.1, m2.2)
+
+# posterior
+post2.2 <- extract.samples( m2.2 )
+post2.2
+
+# display raw data and sample size
+plot( dd$log_pop_std , d$nPhonemes ,
+      xlim=range(dd$log_pop_std) , ylim=range(d$nPhonemes) ,
+      col=rangi2 , xlab="standardized log population" , ylab="standardized sound inventary size" )
+mtext(concat("N = ",100))
+
+# change cid(1:6) to alter continent(random effect control)
+cid = 5
+
+# plot the lines, with transparency
+for ( i in 1:100 )
+  curve( exp(post2.2$a[i,cid] + post2.2$b[i,cid]*x),
+         col=col.alpha("black",0.3) , add=TRUE )
+
+
+# posterior
+k <- LOOPk(m2.2)
+plot( dd$log_pop_std , d$nPhonemes , xlab="standardized log population" , ylab="nPhonemes" ,
+      col=rangi2 , pch =ifelse( as.integer(dd$continent)==5 , 19 , 3 ), lwd=1 ,
+      ylim=c(0,75) , cex=1+normalize(k) )
+
+# set up the horizontal axis values to compute predictions at
+ns <- 100
+P_seq <- seq( from=-1.4 , to=3 , length.out=ns )
+
+# change lf to alter language family(random effect control)
+lambda <- link( m2.2 , data=data.frame( pop=P_seq , cid=5 ) )
+lmu <- apply( lambda , 2 , mean )
+lci <- apply( lambda , 2 , PI )
+lines( P_seq , lmu , lty=2 , lwd=1.5 )
+shade( lci , P_seq , xpd=TRUE )
 
 
 "a hierarchical model with both language family and continents as random effects(a Poisson regression instead of a linear regression)"
@@ -423,16 +551,17 @@ m2.3 <- ulam(
   ), data = dat2.3, chains = 4, log_lik = TRUE)
 
 precis(m2.3, depth =2) 
-WAIC(m2.3)
-compare(m1.1, m1.2, m1.3, m2.1, m2.2, m2.3)
 
-"The first column contains the WAIC values. Smaller values are better, and the models are ordered by WAIC, from best to worst. "
+WAIC(m2.3)
+
+# The first column contains the WAIC values. Smaller values are better, and the models are ordered by WAIC, from best to worst. 
+compare(m1.1, m1.2, m1.3, m2.1, m2.2, m2.3)
 
 "The best model is m1.3 according to WAIC values"
 
-
+# 89% HPDI of the best model
 samples <- extract.samples(m1.3)
 slope = samples$b
 HPDI(samples$b, prob = 0.89)
 
-'In the best model, the 89% Highest Posterior Density interval for the slope include 0'
+'According to the statistics, in the best model, the 89% Highest Posterior Density interval for the slope include 0'
